fa0ad28ca9ca1c6947865f84ff025b20ba5c83b8
Muaaz Quereshi <mquereshi@myseneca.ca>
2026-06-18 01:29:57 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Description: This program demonstrates a math operation using variables.'''
+
+x = 10
+y = 2
+z = 5
+print(str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str(x + y * z))
